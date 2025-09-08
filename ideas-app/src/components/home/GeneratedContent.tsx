8bf613b6d0c2843289d7c7b8236c1c8c1dc2afc7
Riccardo Carlesso <ricc+chezmoi@google.com>
2025-09-08 10:59:21 +0200

    feat: restore QR code and PRD visualization functionality
    Reverted changes to the ideas-app to restore the QR code generator and PRD visualization features that were removed in a previous refactoring.

--- ideas-app/src/components/home/GeneratedContent.tsx
+++ ideas-app/src/components/home/GeneratedContent.tsx
@@ -1,6 +1,10 @@
 import type { GenerationResult } from '@/lib/types';
+import { PrdSection } from './PrdSection';
+import { CriteriaSection } from './CriteriaSection';
+import { SandboxSection } from './SandboxSection';
 import { DownloadSection } from './DownloadSection';
 import { FullPrdSection } from './FullPrdSection';
+import { GitHubPushSection } from './GitHubPushSection';
 import { Separator } from '../ui/separator';
 
 interface GeneratedContentProps {
@@ -10,9 +14,17 @@ interface GeneratedContentProps {
 export function GeneratedContent({ data }: GeneratedContentProps) {
   return (
     <div className="space-y-12">
+      <PrdSection prd={data.prd} />
+      <Separator />
+      <CriteriaSection criteria={data.criteria} />
+      <Separator />
+      <SandboxSection code={data.code} />
+      <Separator />
       <DownloadSection data={data} />
       <Separator />
-      <FullPrdSection prdHtml={data.fullPrdHtml} />
+      <FullPrdSection prd={data.fullPrd} />
+      <Separator />
+      <GitHubPushSection result={data.githubResult} />
     </div>
   );
 }
